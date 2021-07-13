44cb4c50060c0b75df091e00abcc327a81b1545d
kevinDRibeiro <kevinribe@gmail.com>
2021-07-13 20:54:22 -0300

    nome corrigido

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ function ProfileSidebar(propriedades) {
 }
 
 export default function Home() {
-  const usuarioAleatorio = 'omariosouto';
+  const usuarioAleatorio = 'kevinDRibeiro';
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
